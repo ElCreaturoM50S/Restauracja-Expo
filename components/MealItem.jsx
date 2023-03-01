9b5098c118310ff911de8ee97a87cf0274585cf0
ElCreaturoM50S <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-03-01 09:39:45 +0100

    updaejt

---
+++ components/MealItem.jsx
@@ -0,0 +1,54 @@
+import {View, Text, StyleSheet, Pressable, Image} from 'react-native';
+
+export default function MealItem(props){
+    console.log(props)
+    return (
+        <View style={styles.mealItem}>
+            <Pressable>
+                <View>
+                    <Image source={{uri: props.imageUrl}} 
+                        style={styles.image}/>
+                    <Text> {props.title} </Text>
+                </View>
+
+                <View style={styles.details}>
+                    <Text> {props.duration} </Text>
+                    <Text> {props.complexity} </Text>
+                    <Text> {props.affordability} </Text>
+                </View>
+            </Pressable>
+        </View>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        width: '100%',
+        height: 200,
+    },
+    title:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        pading:8,
+    },
+    mealItem:{
+        margin: 16,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        elevation: 4,
+        shadowOffset: {width: 0, height: 2},
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowRadius: 8
+    },
+    details:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    }
+})
--- screen/MealsOverviewScreen.jsx
+++ screen/MealsOverviewScreen.jsx
@@ -4,14 +4,38 @@ import {
     FlatList,
     StyleSheet,
 } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem'; 
 
 export default function MealsOverviewScreen(props){
-
+    console.log("Test")
     const catId = props.route.params.categoryId;
+    console.log(props)
+
+    console.log(catId)
+    const displayMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    })
+
+    console.log(displayMeals)
+    function renderMealItem(itemData){
+        console.log("Test")
+        const mealItemProps = {
+            title: itemData.item.title,
+            imageUrl: itemData.item.imageUrl,
+            complexity: itemData.item.complexity,
+            affordability: itemData.item.affordability
+        }
+        console.log(mealItemProps)
+        return <MealItem {...mealItemProps} />
+    }
 
     return (
         <View>
-            <Text> {catId} </Text>
+            <FlatList data={displayMeals}
+                    keyExtractor={(item) => item.id}
+                    renderItem={renderMealItem}
+                    />
         </View>
     )
 }
